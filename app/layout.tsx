ead1aeb90466d08c0bd19dcb63a573b646857300
hardikvyazz <hardikvyas.dev@gmail.com>
2025-04-28 16:38:13 +0530

    Vercel Analytics and SpeedInsights addded

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import {ThemeProvider} from "./provider";
+import { Analytics } from "@vercel/analytics/react"
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -23,8 +25,10 @@ export default function RootLayout({
       <body className={inter.className}>
         <ThemeProvider attribute="class" defaultTheme="dark" enableSystem disableTransitionOnChange>
         {children}
-        </ThemeProvider>
-        </body>
+          </ThemeProvider>
+          <SpeedInsights />
+        <Analytics />
+      </body>
     </html>
   );
 }
